ac91a95a45d173f4746ff44c730232d3e43cda64
dev-laww <lawrenceallen.tech@gmail.com>
2024-09-03 23:32:49 +0800

    feat: theme update

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,3 +1,20 @@
 import { createTheme } from '@mantine/core';
 
-export const theme = createTheme({});
+export const theme = createTheme({
+    primaryColor: 'puceRed',
+    primaryShade: 9,
+    colors: {
+        puceRed: [
+            '#faf0f2',
+            '#eddde1',
+            '#deb7bf',
+            '#d08f9d',
+            '#c56d7f',
+            '#bd586d',
+            '#bb4d63',
+            '#a53f52',
+            '#933649',
+            '#822b3e'
+        ]
+    }
+});
