4efc42f8eacb407be124c2b2bc88e00bc3a5a90e
dev-laww <lawrenceallen.tech@gmail.com>
2024-09-08 20:37:14 +0800

    feat: created scroll to bottom behavior

--- src/components/pages/chat.tsx
+++ src/components/pages/chat.tsx
@@ -1,26 +1,36 @@
 'use client'
 
 import { Box, Container, Flex, Group, ScrollArea } from '@mantine/core';
-import React, { useEffect, useRef } from 'react';
-import { useMediaQuery } from '@mantine/hooks';
+import React, { useEffect, useRef, useState } from 'react';
 import { Message } from '@components/forms';
+import { useElementSize } from '@mantine/hooks';
 
 
 const Chat = () => {
     const viewport = useRef<HTMLDivElement>(null);
-    const matches = useMediaQuery('(max-width: 48em)');
+    const [ content, setContent ] = useState<React.ReactNode[]>([]);
+    const { ref, height } = useElementSize()
 
     useEffect(() => {
-        viewport.current?.scrollTo({ top: viewport.current!.scrollHeight * 2, behavior: 'smooth' })
-    }, [])
+        const { scrollHeight, clientHeight, scrollTop } = viewport.current!
+        // TODO: Fix this
+
+        viewport.current?.scrollTo({ top: scrollHeight, behavior: 'smooth' })
+    }, [ height ])
 
 
     return (
         <Group h='100%' grow>
             <Flex direction='column' h='100%' miw={ { base: '100%', sm: '60%' } } justify='flex-end'>
-                <ScrollArea viewportRef={ viewport }>
-                    <Container size='sm' py='lg'>
-
+                <ScrollArea
+                    viewportRef={ viewport }
+                    scrollbarSize={ 3 }
+                    offsetScrollbars
+                    id='scroll'
+                    onScroll={ event => console.log('scrolling', event) }
+                >
+                    <Container size='sm' py='lg' ref={ ref }>
+                        {/*    content here   */ }
                     </Container>
                 </ScrollArea>
                 <Box w='100%' mb='xl'>
