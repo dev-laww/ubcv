39f56a3ddf727c169a852dc3ae386c0003904e54
dev-laww <lawrenceallen.tech@gmail.com>
2024-09-10 02:48:55 +0800

    feat: created context provider

---
+++ src/lib/llm/context.ts
@@ -0,0 +1,37 @@
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { WebPDFLoader } from '@langchain/community/document_loaders/web/pdf';
+import { OllamaEmbeddings } from '@langchain/ollama';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { Embeddings } from '@langchain/core/embeddings';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+
+class Context {
+    static async loadFromLoader(loader: any, embeddings: Embeddings) {
+        const docs = await loader.load();
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 700,
+            chunkOverlap: 100
+        });
+        const splits = await textSplitter.splitDocuments(docs);
+        const vectorStore = await MemoryVectorStore.fromDocuments(splits, embeddings);
+
+
+        return vectorStore.asRetriever({ k: 2 });
+    }
+
+    static async load(file: Blob) {
+        const loader = new WebPDFLoader(file);
+        const embeddings = new OllamaEmbeddings();
+
+        return Context.loadFromLoader(loader, embeddings);
+    }
+
+    static async loadLocal(file: string) {
+        const loader = new PDFLoader(file);
+        const embeddings = new OllamaEmbeddings();
+
+        return Context.loadFromLoader(loader, embeddings);
+    }
+}
+
+export { Context };
