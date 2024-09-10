3aad5c7bc62e529e5790c67be1ad6e76261a96c0
dev-laww <lawrenceallen.tech@gmail.com>
2024-09-11 00:19:21 +0800

    fix: url not being active

--- src/components/common/navigation/conversation.tsx
+++ src/components/common/navigation/conversation.tsx
@@ -2,26 +2,27 @@
 
 import classes from './component.module.css';
 import React from 'react';
-import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 import { Conversation as Item } from '@prisma/client';
+import { useWindowLocation } from '@hooks';
 
 interface ConversationProps {
     data: Item;
 }
 
 const Conversation: React.FC<Readonly<ConversationProps>> = ({ data }) => {
-    const pathname = usePathname();
     const url = `/chat?thread=${ data.id }`;
+    const windowLocation = useWindowLocation();
 
     // TODO: Add title prompt
     return (
-        <a
+        <Link
             className={ classes.link }
-            data-active={ pathname === url || undefined }
+            data-active={ windowLocation === url || undefined }
             href={ url }
         >
             <span>{ data.id }</span>
-        </a>
+        </Link>
     )
 }
 
