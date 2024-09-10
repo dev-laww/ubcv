3f8f30b8599eafc7b79c9790ed2a9fa4e558fee4
dev-laww <lawrenceallen.tech@gmail.com>
2024-09-10 23:48:27 +0800

    feat: initial send action

--- src/components/forms/message.tsx
+++ src/components/forms/message.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ActionIcon, TextInput } from '@mantine/core';
 import { IconSend2 } from '@tabler/icons-react';
-import { message } from '@actions/message'
+import { useForm } from '@mantine/form';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { create, message } from '@actions/message';
 
 const Message: React.FC = () => {
+    const [ loading, setLoading ] = useState(false)
+    const searchParams = useSearchParams()
+    const router = useRouter()
+    const form = useForm({
+        initialValues: { message: '' }
+    })
+
+    const submit = async (values: typeof form.values) => {
+        if (!values.message || loading) return
+
+        form.reset()
+        console.log(values)
+        setLoading(true)
+
+        try {
+            const thread = searchParams.get('thread')
+
+            let id: string = ''
+
+            if (!thread) {
+                id = await create()
+
+                router.replace(`/chat?thread=${ id }`)
+            }
+
+            const response = await message({ thread: thread || id, input: values.message })
+
+            console.log(response)
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
-        <form action={ message }>
+        <form onSubmit={ form.onSubmit(submit) }>
             <TextInput
+                { ...form.getInputProps('message') }
+                autoComplete='off'
                 placeholder='Type a message'
                 rightSection={ (
-                    <ActionIcon color='puceRed' variant='transparent' type='submit'>
+                    <ActionIcon color='puceRed' variant='transparent' type='submit' loading={ loading }>
                         <IconSend2 />
                     </ActionIcon>
                 ) }
--- src/lib/actions/message.ts
+++ src/lib/actions/message.ts
@@ -1,7 +1,30 @@
 'use server'
 
-const message = async () => {
-    console.log('Send message')
+import { auth } from '@lib/auth';
+import { Chain } from '@lib/llm/chain';
+import { prisma } from '@lib/prisma';
+
+interface MessageOptions {
+    thread: string;
+    input: string;
 }
 
-export { message }
+const create = async () => {
+    const { user } = await auth() || {}
+
+    return prisma.conversation.create({
+        data: {
+            user: { connect: { email: user!.email! } }
+        }
+    }).then(({ id }) => id)
+}
+
+const message = async ({ thread, input }: MessageOptions) => {
+    const session = await Chain.session(thread)
+
+    const response = await session.invoke(input)
+
+    return response.answer
+}
+
+export { create, message }
