67e14b89c97f7b4a18b2a563e9f68593685854b7
dev-laww <lawrenceallen.tech@gmail.com>
2024-09-18 19:21:27 +0800

    feat: added title column

--- prisma/migrations/20240918102439_init/migration.sql
+++ prisma/migrations/20240918102439_init/migration.sql
@@ -35,6 +35,7 @@ CREATE TABLE "UserSettings" (
 CREATE TABLE "Conversation" (
     "id" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
+    "title" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
