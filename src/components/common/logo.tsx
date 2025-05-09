ceffb52dd3ac694543eeba6c4e44649a7bdc3e46
dev-laww <lawrenceallen.tech@gmail.com>
2025-05-09 10:44:09 +0800

    feat: added size props to logo

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -5,14 +5,16 @@ import React from 'react';
 interface LogoProps {
     className?: string;
     full?: boolean;
+    width?: number;
+    height?: number;
     c?: StyleProp<MantineColor>
 }
 
-const Logo: React.FC<Readonly<LogoProps>> = ({ className, full, c }) => {
+const Logo: React.FC<Readonly<LogoProps>> = ({ className, full, c, width = 100, height = 100 }) => {
     return (
         <Group className={ className } grow w='100%' c={ c }>
             <Box maw={ full && '30%' || undefined }>
-                <Image src='/logo.svg' alt='logo' width={ 100 } height={ 100 } />
+                <Image src='/logo.svg' alt='logo' width={ width } height={ height } />
             </Box>
             { full && <Title order={ 3 }>E - UBCV</Title> }
         </Group>
